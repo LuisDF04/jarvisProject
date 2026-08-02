a974336dc1da1b03695d968345c73f0a2caa0003
LuisDF04 <146751915+LuisDF04@users.noreply.github.com>
2026-08-02 22:06:48 +0100

    correction plugins

--- src/plugins_library/saludar_asistente.py
+++
@@ -1,7 +0,0 @@
-def saludar_asistente(parameter):
-    if parameter is None:
-        return "Estoy funcionando correctamente, gracias por preguntar"
-    elif parameter.lower() == "como estas":
-        return "Estoy funcionando correctamente, gracias por preguntar"
-    else:
-        return "No entiendo tu pregunta, ¿podrías repetirla?"
